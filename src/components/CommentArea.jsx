8270c701fda1022b079921b6e55bebc9b6560a91
GhostPeppino2612 <giuseacqua6@gmail.com>
2024-09-29 10:01:29 +0200

    first commit

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,61 @@
+import { Component, useEffect, useState } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+
+const CommentArea = (props) => {
+  // state = {
+  //   comments: [],
+  //   isLoading: false,
+  //   isError: false,
+  // }
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
+          {
+            headers: {
+              Authorization: "Bearer inserisci-qui-il-tuo-token",
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          setComments(comments);
+          setIsLoading(false);
+          setIsError(false);
+        } else {
+          console.log("error");
+          setIsLoading(false);
+          setIsError(true);
+        }
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    };
+    if (props.asin) {
+      setIsLoading(true);
+      fetchComments();
+    }
+  }, [props.asin]);
+
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
+
+export default CommentArea;
